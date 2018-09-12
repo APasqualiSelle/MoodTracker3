03ed685d5354cdd37a99be740c4528e630efc6c9
APasqualiSelle <alinealexandria@gmail.com>
2018-09-12 13:07:14 +0200

    Initialized buttons Added two arrays: one for the happy moods and the other for the sad ones

--- app/src/main/java/com/alineselle/moodtracker3/MainActivity.java
+++ app/src/main/java/com/alineselle/moodtracker3/MainActivity.java
@@ -2,12 +2,27 @@ package com.alineselle.moodtracker3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button mButtonPlus;
+    private Button mButtonMinus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonPlus = findViewById(R.id.plus_button);
+        mButtonMinus = findViewById(R.id.minus_button);
+
+        int[] imagesIdForPlusButton = {R.drawable.smiley_normal,
+                R.drawable.smiley_happy,
+                R.drawable.smiley_super_happy};
+
+        int[] imagesIdsForMinusButton = {R.drawable.smiley_disappointed,
+                R.drawable.smiley_sad};
     }
 }
