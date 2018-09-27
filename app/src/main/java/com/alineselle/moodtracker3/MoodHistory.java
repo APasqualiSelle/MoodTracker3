b02dc265fc9ecdb9d351b36d095d3490927ed843
APasqualiSelle <alinealexandria@gmail.com>
2018-09-27 12:15:06 +0200

    At the MoodHistory activity:
    Created the variable from type 'int' = 'mStoreLastColor' to retrieve the value from the prefences'key PREF_KEY_LAST_COLOR created at Main Activity
    created the variable mDisplayColorMoodChosen from type ImageView in order to display the background color retrieved with mStoreLastColorChosen

--- app/src/main/java/com/alineselle/moodtracker3/MoodHistory.java
+++ app/src/main/java/com/alineselle/moodtracker3/MoodHistory.java
@@ -4,7 +4,6 @@ import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
-import android.widget.TableRow;
 import android.widget.TextView;
 
 public class MoodHistory extends AppCompatActivity {
@@ -13,12 +12,8 @@ public class MoodHistory extends AppCompatActivity {
     private String mStoreComment;
     private ImageView mDisplayColorMoodChosen;
 
-  private int mStoreLastColorChose;
-    private int mStoreColorMoodIndex0;
-    private int mStoreColorMoodIndex1;
-    private int mStoreColorMoodIndex2;
-    private int mStoreColorMoodIndex3;
-    private int mStoreColorMoodIndex4;
+  private int mStoreLastColorChosen;
+
 
 
     private SharedPreferences mHistoryResultPreferences;
@@ -32,25 +27,16 @@ public class MoodHistory extends AppCompatActivity {
         mHistoryResultPreferences = getSharedPreferences(MainActivity.PREF_KEY_HISTORY_MOOD, MODE_PRIVATE);
         mDisplayColorMoodChosen = findViewById(R.id.chosenMoodColor1);
 
-        mStoreLastColorChose = mHistoryResultPreferences.getInt(MainActivity.PREF_KEY_LAST_COLOR, 0);
-        /*mStoreColorMoodIndex0 = mHistoryResultPreferences.getInt(MainActivity.PREF_KEY_FADED_RED, 0);
-        mStoreColorMoodIndex0 = mHistoryResultPreferences.getInt(MainActivity.PREF_KEY_WARM_GREY, 0);
-        mStoreColorMoodIndex0 = mHistoryResultPreferences.getInt(MainActivity.PREF_KEY_CORNFLOWER_BLUE, 0);
-        mStoreColorMoodIndex0 = mHistoryResultPreferences.getInt(MainActivity.PREF_KEY_LIGHT_SAGE, 0);
-        mStoreColorMoodIndex0 = mHistoryResultPreferences.getInt(MainActivity.PREF_KEY_BANANA_YELLOW, 0);
+        mStoreLastColorChosen = mHistoryResultPreferences.getInt(MainActivity.PREF_KEY_LAST_COLOR, 0);
 
-        *//*mDisplayColorMoodChosen.setImageResource(mStoreColorMoodIndex1);
-        mDisplayColorMoodChosen.setImageResource(mStoreColorMoodIndex2);
-        mDisplayColorMoodChosen.setImageResource(mStoreColorMoodIndex4);
-        mDisplayColorMoodChosen.setImageResource(mStoreColorMoodIndex3);*/
 
         mStoreComment = mHistoryResultPreferences.getString(MainActivity.PREF_KEY_COMMENT, null);
         mDisplayComment = findViewById(R.id.commentText);
         mDisplayComment.setText(mStoreComment);
 
-       // mDisplayColorMoodChosen.setImageResource(mStoreColorMoodIndex0);
 
-        mDisplayColorMoodChosen.setImageResource(mStoreLastColorChose);
+
+        mDisplayColorMoodChosen.setImageResource(mStoreLastColorChosen);
 
     }
 }
